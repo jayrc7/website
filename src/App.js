6847a1f634c74f2f47d425ff94dc2977a276a75a
Jason Cabrera <cabrerajason7@gmail.com>
2018-09-30 12:16:45 -0700

    change

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
            <h2 className = "below-title"> Sophomore at University of California, San Diego: B.S Mathematics - Computer Science </h2>
            <img className = "front-logo" src = "https://cdn.dribbble.com/users/109649/screenshots/954354/lightningbolt.png" />
            <p> here is some text welcome </p>`
+           <h1 className = "title"> Let's go </h1>
 
       </div>
 
