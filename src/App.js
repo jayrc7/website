91cf6fae2953f5df49ab4720d78c2bb1bda850f9
Jason Cabrera <cabrerajason7@gmail.com>
2018-09-30 12:41:27 -0700

    about me

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
           <h1 className = "title"> Jason Cabrera</h1>
            <h2 className = "below-title"> Sophomore at University of California, San Diego: B.S Mathematics - Computer Science </h2>
            <img className = "front-logo" src = "https://cdn.dribbble.com/users/109649/screenshots/954354/lightningbolt.png" />
-           <h1 className = "next-picture"> Let's go </h1>
+           <h1 className = "next-picture"> About Me</h1>
 
       </div>
 
