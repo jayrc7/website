e4ec7bee77df3e0a29ed3092fccb96b651ab2f97
Jason Cabrera <cabrerajason7@gmail.com>
2018-09-30 11:33:03 -0700

    change

--- src/App.js
+++ src/App.js
@@ -5,16 +5,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-      <div>
+      <div className = "App">
           <h1 className = "title"> Jason Cabrera</h1>
-           <h2 className = "below-title"> Sophomore at University of California, San Diego: B.S Mathematics - Computer Science </h2> 
-           <div className = "App">
+           <h2 className = "below-title"> Sophomore at University of California, San Diego: B.S Mathematics - Computer Science </h2>
            <img className = "front-logo" src = "https://cdn.dribbble.com/users/109649/screenshots/954354/lightningbolt.png" />
-        <body></body>
-        </div>
+           <p> here is some text </p>
+
       </div>
-      </div>
+
     );
   }
 }
