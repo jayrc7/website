3288e67f13d4298fec7a7455a91d61d25aac62ce
Jason Cabrera <cabrerajason7@gmail.com>
2018-10-01 00:54:36 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar.js';
 class App extends Component {
   render() {
     return (
@@ -13,9 +13,15 @@ class App extends Component {
            <h2 className = "description"> Hello I'm Jason and welcome to my website! Im a programmer, sports enthusiast, and nature lover currently living in Baldwin Park, California. On my free time 
            I tackle LeetCode challenges and look for more ways to gain programming experience. I find a lot of joy in full stack development and I'm always up for new challenges in coding and in life. My other hobbies consist
            of spending time with friends and family, playing soccer, and watching tv shows. </h2>
-        <h2 className = "description">
-        "You can't cross the sea merely by standing and staring at the water." - Rabindranath Tagore 
-        </h2>
+
+           <div className = "for-nav">
+               <a href = "mailto:jrcabrer@ucsd.edu" title = "Email"target = "_blank"><img className = "email" src = "https://ubisafe.org/images/email-vector.png" /> </a>
+                <a href= "https://github.com/jayrc7" target = "_blank" title = "GitHub"><img className = "git"src="https://4.bp.blogspot.com/--omgjr2n6eg/WyN-i8kG6PI/AAAAAAAASGc/4rW8yeCYWPogpTdE9Y50ry2vcFy_l4rjQCLcBGAs/s320/github.jpg"/></a>
+                <a href = "https://www.linkedin.com/in/jason-cabrera-b45522167/" target = "_blank" title = "LinkedIn"> <img className = "linked" src = "https://2.bp.blogspot.com/-RDbj7QYLEog/WyODFhyIcBI/AAAAAAAASGo/Q3UFQ8u-RlQhhOkY8843Z6i1Xks1ywU8gCLcBGAs/s320/linkedin.png" /> </a>
+
+                 <a href = "https://docs.google.com/document/d/1QS9l3O1V1auV03aBINOeANODck8z9lPR6S0G3Mwr2_M/edit?usp=sharing" target = "_blank" title = "Resume"> <img className = "resume" src = "https://cdn3.iconfinder.com/data/icons/web-ui-3/128/Menu2-2-512.png" /> </a>
+
+           </div>
 
       </div>
 
