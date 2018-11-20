24f77abee2ffcb733b757178ee059033b0b25997
Jason Cabrera <30588403+jayrc7@users.noreply.github.com>
2018-11-20 01:46:07 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
            <h2 className = "below-title"> Sophomore at University of California, San Diego: B.S. Mathematics - Computer Science </h2>
            <img className = "front-logo" src = "https://cdn.dribbble.com/users/109649/screenshots/954354/lightningbolt.png" />
            <h1 className = "next-picture"> About Me</h1>
-           <h2 className = "description"> Hello I'm Jason and I'm a programmer, sports enthusiast, and nature lover currently living in Baldwin Park, California. I spend my free time playing soccer, watching tv shows, and looking for more ways to gain programming experience. I'm always up for new challenges and I find a lot of joy in full stack development</h2>
+           <h2 className = "description"> Hello I'm Jason and I'm a programmer, sports enthusiast, and nature lover currently living in Baldwin Park, California. I spend my free time playing soccer, watching tv shows, and looking for more ways to gain programming experience. I'm always up for new challenges and I find a lot of joy in full stack development.</h2>
             
            <div className = "contain">
            <div className = "for-nav">
